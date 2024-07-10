6b3139e0c28d5436959947f41cea3948a2b532ea
lzm <2854588717@qq.com>
2024-07-10 13:59:46 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
+<script>
+import { RouterView } from 'vue-router'
+import Headers from './components/Header/Header.vue'
+import Footer from './components/Footer/Footer.vue'
+
+//注册页面
+export default {
+  components: { Headers, Footer, RouterView },
+
+}
+
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Headers />
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
   <router-view/>
+  <Footer />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
